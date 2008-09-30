d257375736edad27cb33ed7505c612d7745a965c
Emmanuel Bouton <emmanuel@bouton.biz>
2008-10-01 00:24:51 +0200

    Capistrano configuration

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "teamtracking"
+set :repository,  "sources.homelinux.net:/var/git/teamtracking.git"
+set :domain, "teamtracking.homelinux.net"
+
+set :scm, :git
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1
+
+ssh_options[:paranoid] = false
+
+set :deploy_to, "/usr2/web/#{application}"
+set :use_sudo, false
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+task :update_config, :roles => [:app] do
+  run "cp -Rf #{shared_path}/config/* #{release_path}/config/"
+end
+after "deploy:update_code", :update_config
+
+# Override start task
+deploy.task :start do
+  # nothing
+end
+
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+after :deploy, "passenger:restart"
