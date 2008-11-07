c72b945c41f0147f05809755085c2e785d82b72c
ebouton <ebouton@ebouton1.(none)>
2008-11-07 19:23:29 +0100

    Add javascript behaviour

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,23 @@ module ApplicationHelper
     classes += " other_month" if date.month != current_month
     classes
   end
+
+  def morning_id_of(date)
+    "'am#{date.to_s(:number)}'"
+  end
+
+  def afternoon_id_of(date)
+    "'pm#{date.to_s(:number)}'"
+  end
+
+  def ids_of_day(date)
+    "$A([#{morning_id_of(date)}, #{afternoon_id_of(date)}])"
+  end
+
+  def ids_of_week(week)
+    result = "$A(["
+    week.each { |date| result += "#{morning_id_of(date)},#{afternoon_id_of(date)}," }
+    result[result.length-1] = "]"
+    result + ")"
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,39 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function check(id)
+{
+  $(id).checked = true;
+}
+
+function uncheck(id)
+{
+  $(id).checked = false;
+}
+
+function toggle_check(id)
+{
+  $(id).checked = !$(id).checked;
+}
+
+function check_all(list)
+{
+  list.each(function (id) { check(id) });
+}
+
+function uncheck_all(list)
+{
+  list.each(function (id) { uncheck(id) });
+}
+
+function toggle_check_all(list)
+{
+  var first_unchecked = list.find(function (id) { return $(id).checked == false });
+  if ( first_unchecked == undefined ) {
+    uncheck_all(list);
+  }
+  else {
+    check_all(list);
+  }
+}
+
